e05fcab0a0a20df14692c1b520f87b3b17d13906
Anefu <einsteinanefu@gmail.com>
2022-01-27 15:13:21 +0100

    edited configs

--- modules/infra/files/setup-web.sh
+++ modules/infra/files/setup-web.sh
@@ -6,17 +6,54 @@ mkdir -p /var/www/html
 systemctl start nginx
 touch  /var/www/html/healthstatus
 
-cat << EOF > /etc/nginx/conf.d/reverse.conf
-server {
-    listen       80;
+cat << EOF > /etc/nginx/nginx.conf
+user nginx;
+worker_processes auto;
+error_log /var/log/nginx/error.log;
+pid /run/nginx.pid;
 
-    location /healthstatus {
+# Load dynamic modules. See /usr/share/doc/nginx/README.dynamic.
+include /usr/share/nginx/modules/*.conf;
+
+events {
+    worker_connections 1024;
+}
+
+http {
+    log_format  main  '\$remote_addr - \$remote_user [$time_local] "\$request" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
+                      '"\$http_user_agent" "\$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
+    sendfile            on;
+    tcp_nopush          on;
+    tcp_nodelay         on;
+    keepalive_timeout   65;
+    types_hash_max_size 2048;
+     client_max_body_size 100M;
+    
+    default_type        application/octet-stream;
+
+    # Load modular configuration files from the /etc/nginx/conf.d directory.
+    # See http://nginx.org/en/docs/ngx_core_module.html#include
+    # for more information.
+    include /etc/nginx/conf.d/*.conf;
+
+     server {
+        listen       80;
+        root          /var/www/;
+        #server_name  ;
+
+        location /healthstatus {
         access_log off;
         return 200;
-    }
-     
-    location / {
-        proxy_pass                   http://localhost:8080/; 
+       }
+    
+         
+        location /hello {
+            proxy_pass                   https://127.0.0.1/hello/; 
+           }
     }
 }
 EOF
